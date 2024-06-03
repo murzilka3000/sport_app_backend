c50efcaa03591af8cc9adac22a77dd591d07b6b6
Nikolay <nikolayzhidkov@icloud.com>
2024-06-03 07:12:12 +0300

    start server

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,3 @@
+export const authUser = async (req, res) => {
+    res.json({message: 'Ты авторизировался'})
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import authRoutes from './app/auth/auth.routes.js'
+import 'colors'
+
+const app = express()
+
+async function main() {
+    // if(process.env.NODE_ENV === 'development') {
+    //     return (
+    //         app.use(morgan('dev'))
+    //     )
+    // }
+    app.use(express.json())
+    app.use('/api/auth', authRoutes)
+
+    const PORT = 5000
+
+    app.listen(
+        PORT,
+        console.log(
+            `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold
+        )
+    )
+}
+
+main()
